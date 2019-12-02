7bf7f445402d5b36b58d1d7cdf27027f1fdff1bb
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-02 22:59:08 +0100

    moving stuff around

---
+++ dec2/__init__.py
@@ -0,0 +1,9 @@
+class SantaVM:
+    def __init__(self, punchcard: str) -> None:
+        super().__init__()
+
+    def run(self):
+        pass
+
+    def core_dump(self):
+        return "core dump"
