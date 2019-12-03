9a7bafe01ed92d0545ede7220a28ef3bf276e54a
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-03 17:21:24 +0100

    Simplify

--- dec2/__init__.py
+++ dec2/__init__.py
@@ -7,33 +7,39 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
 class SantaVM:
     def __init__(self, punchcard: str) -> None:
         super().__init__()
         self.core = [int(code) for code in punchcard.split(",")]
 
-    def execute(self):
-        instruction_pointer = 0
-        halt = False
-        while not halt:
-            instructions = list(chunks(self.core, 4))
+    def execute(self, input1: int = None, input2: int = None):
+        if input1 is not None:
+            self.core[1] = input1
+
+        if input2 is not None:
+            self.core[2] = input2
+
+        def add(value1_ref, value2_ref):
+            return self.core[value1_ref] + self.core[value2_ref]
+
+        def mul(value1_ref, value2_ref):
+            return self.core[value1_ref] * self.core[value2_ref]
+
+        for instruction_pointer in range(0, len(self.core), 4):
             try:
-                instruction, parameter1, parameter2, parameter3 = instructions[instruction_pointer]
+                instruction, parameter1, parameter2, parameter3 = self.core[instruction_pointer:instruction_pointer + 4]
                 if instruction is 1:
-                    result = self.core[parameter1] + self.core[parameter2]
+                    result = add(parameter1, parameter2)
                     self.core[parameter3] = result
                 elif instruction is 2:
-                    result = self.core[parameter1] * self.core[parameter2]
+                    result = mul(parameter1, parameter2)
                     self.core[parameter3] = result
                 elif instruction is 99:
-                    halt = True
-                instruction_pointer = instruction_pointer + 1
+                    break
             except ValueError:
-                if self.core[instruction_pointer][0] is 99:
-                    halt = True
+                if self.core[instruction_pointer] is 99:
+                    break
 
     def core_dump(self):
         return ",".join([str(intcode) for intcode in self.core])
-
-
-
