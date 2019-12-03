262b0a0642779bc93e8052718a5bfd2b3293c374
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-03 16:51:11 +0100

    Simplify

--- dec2/__init__.py
+++ dec2/__init__.py
@@ -1,65 +1,39 @@
 import itertools
 from typing import List
 
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 class SantaVM:
     def __init__(self, punchcard: str) -> None:
         super().__init__()
         self.core = [int(code) for code in punchcard.split(",")]
-        self.opCodes = []
-        buffer = []
 
-        for intcode in self.core:
-            if intcode is 99 and len(self.opCodes) is 0:
-                self.opCodes.append(OpCode(self, buffer))
-                self.opCodes.append(OpCode(self, [99]))
-            elif intcode is 99 and self.opCodes[-1].is_instruction():
-                self.opCodes.append(OpCode(self, buffer))
-                self.opCodes.append(OpCode(self, [99]))
-                buffer.clear()
-            else:
-                buffer.append(intcode)
-        self.opCodes.append(OpCode(self, buffer))
-
-    def __getitem__(self, item):
-        return self.core[item]
-
-    def run(self):
-        try:
-            [opCode() for opCode in self.opCodes]
-        except StopIteration:
-            pass
+    def execute(self):
+        instruction_pointer = 0
+        halt = False
+        while not halt:
+            instructions = list(chunks(self.core, 4))
+            try:
+                instruction, parameter1, parameter2, parameter3 = instructions[instruction_pointer]
+                if instruction is 1:
+                    result = self.core[parameter1] + self.core[parameter2]
+                    self.core[parameter3] = result
+                elif instruction is 2:
+                    result = self.core[parameter1] * self.core[parameter2]
+                    self.core[parameter3] = result
+                elif instruction is 99:
+                    halt = True
+                instruction_pointer = instruction_pointer + 1
+            except ValueError:
+                if self.core[instruction_pointer][0] is 99:
+                    halt = True
 
     def core_dump(self):
-        return ','.join([str(intcode) for intcode in self.core])
+        return ",".join([str(intcode) for intcode in self.core])
 
 
-class OpCode:
-    def __init__(self, vm: SantaVM, value: List[int]) -> None:
-        super().__init__()
-        self.vm = vm
-        self.value = value[:]
 
-    def is_instruction(self):
-        if self.value[0] is 99:
-            return True
-        return len(self.value) == 4
-
-    def __call__(self, *args, **kwargs):
-        if self.value[0] is 1:
-            return self.execute_addition(self.value[1], self.value[2], self.value[3])
-        elif self.value[0] is 2:
-            return self.execute_multiplication(self.value[1], self.value[2], self.value[3])
-        elif self.value[0] is 99:
-            raise StopIteration
-        raise RuntimeError(f"{self.value[0]}: not acceptable")
-
-    def __repr__(self) -> str:
-        return repr(self.value)
-
-    def execute_addition(self, value1_location, value2_location, result_reference):
-        result = self.vm.core[value1_location] + self.vm.core[value2_location]
-        self.vm.core[result_reference] = result
-
-    def execute_multiplication(self, value1_location, value2_location, result_reference):
-        result = self.vm.core[value1_location] * self.vm.core[value2_location]
-        self.vm.core[result_reference] = result
--- dec2/test_dec2.py
+++ dec2/test_dec2.py
@@ -13,7 +13,7 @@ import pytest
 )
 def test_expectations(input, expectation):
     vm = SantaVM(input)
-    vm.run()
+    vm.execute()
     assert expectation == vm.core_dump()
 
 
@@ -21,5 +21,12 @@ def test_off_by_one():
     input = "2,4,4,5,99,0"
     expectation = "2,4,4,5,99,9801"
     vm = SantaVM(input)
-    vm.run()
+    vm.execute()
     assert expectation == vm.core_dump()
+
+
+def test_puzzle():
+    punchcard = '1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,2,19,13,23,1,23,10,27,1,13,27,31,2,31,10,35,1,35,9,39,1,39,13,43,1,13,43,47,1,47,13,51,1,13,51,55,1,5,55,59,2,10,59,63,1,9,63,67,1,6,67,71,2,71,13,75,2,75,13,79,1,79,9,83,2,83,10,87,1,9,87,91,1,6,91,95,1,95,10,99,1,99,13,103,1,13,103,107,2,13,107,111,1,111,9,115,2,115,10,119,1,119,5,123,1,123,2,127,1,127,5,0,99,2,14,0,0'
+    vm = SantaVM(punchcard)
+    vm.execute()
+    print(vm.core_dump())
