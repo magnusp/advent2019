4c2460de4e88193ced7dd54833c165fd7107687a
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-06 01:02:12 +0100

    Not really sure what im doing any more :/

--- dec3/__init__.py
+++ dec3/__init__.py
@@ -1,82 +1,42 @@
-from functools import reduce
+import itertools
 from typing import Set
 
 
-def extents(input):
-    bug = [0, 0]
-    minX = 0
-    minY = 0
-    maxX = 0
-    maxY = 0
-
-    for movement in input.split(','):
-        bugX, bugY = bug
-        direction, distance = [movement[0], int(movement[1:])]
-        if direction is 'U':
-            bugY = bugY + distance
-        elif direction is 'D':
-            bugY = bugY - distance
-        elif direction is 'L':
-            bugX = bugX - distance
-        elif direction is 'R':
-            bugX = bugX + distance
-        else:
-            raise RuntimeError(f"Bug cant move that way: {direction}")
-
-        minX = min(bugX, minX)
-        maxX = max(bugX, maxX)
-        minY = min(bugY, minY)
-        maxY = max(bugY, maxY)
-        bug = [bugX, bugY]
-
-    return [(maxX, maxY), (minX, minY)]
-
-
-def max_grid(layout1, layout2):
-    res1 = map(
-        lambda pair: max(pair, key=abs),
-        zip(layout1[0], layout2[0])
+def use_hex(wiring, x: int, y: int, travelled: int) -> tuple:
+    maybe_tuple = filter(
+        lambda t: t[0] == x and t[1] == y,
+        wiring
     )
-    res2 = map(
-        lambda pair: max(pair, key=abs),
-        zip(layout1[1], layout2[1])
-    )
-    return [tuple(res1), tuple(res2)]
-
-
-def dimension(grid):
-    maxRange, minRange = grid
-    return [maxRange[1] - minRange[1], maxRange[0] - minRange[0]]
-
-
-def bug_offset(grid):
-    _, minRange = grid
-    return [abs(minRange[0]), abs(minRange[1])]
+    try:
+        return next(maybe_tuple)
+    except StopIteration:
+        return x, y, travelled
 
 
 def setrogify_wiring(directions):
-    wiring = {(0, 0)}
-    bug = (0, 0)
+    wiring = {(0, 0, 0)}
+    bug = (0, 0, 0)
+
     for movement in directions.split(','):
-        bug_x, bug_y = bug
+        bug_x, bug_y, distance_traveled = bug
         direction, distance = [movement[0], int(movement[1:])]
         line = ()
         if direction is 'U':
-            line = [(bug_x, q) for q in range(bug_y, bug_y + distance + 1)]
+            line = [use_hex(wiring, bug_x, q, p) for q, p in zip(range(bug_y, bug_y + distance + 1), itertools.count(distance_traveled))]
             bug_y = bug_y + distance
         elif direction is 'D':
-            line = [(bug_x, q) for q in range(bug_y, bug_y - distance - 1, -1)]
+            line = [use_hex(wiring, bug_x, q, p) for q, p in zip(range(bug_y, bug_y - distance - 1, -1), itertools.count(distance_traveled))]
             bug_y = bug_y - distance
         elif direction is 'L':
-            line = [(q, bug_y) for q in range(bug_x, bug_x - distance - 1, -1)]
+            line = [use_hex(wiring, q, bug_y, p) for q, p in zip(range(bug_x, bug_x - distance - 1, -1), itertools.count(distance_traveled))]
             bug_x = bug_x - distance
         elif direction is 'R':
-            line = [(q, bug_y) for q in range(bug_x, bug_x + distance + 1)]
+            line = [use_hex(wiring, q, bug_y, p) for q, p in zip(range(bug_x, bug_x + distance + 1), itertools.count(distance_traveled))]
             bug_x = bug_x + distance
         else:
             raise RuntimeError(f"Bug cant move that way: {direction}")
         wiring = wiring.union(line)
-        bug = (bug_x, bug_y)
+        bug = (bug_x, bug_y, distance_traveled + distance)
 
     return wiring
 
--- dec3/test_dec3.py
+++ dec3/test_dec3.py
@@ -1,3 +1,6 @@
+import functools
+from collections import defaultdict
+
 import pytest
 import dec3
 
@@ -5,48 +8,67 @@ import dec3
 @pytest.mark.parametrize(
     'test, expectation',
     [
-        ("U2", {(0, 0), (0, 1), (0, 2)}),
-        ("D2", {(0, 0), (0, -1), (0, -2)}),
-        ("L2", {(0, 0), (-1, 0), (-2, 0)}),
-        ("R2", {(0, 0), (1, 0), (2, 0)}),
-        ("U2,D2", {(0, 0), (0, 1), (0, 2)}),
-        ("D2,U2", {(0, 0), (0, -1), (0, -2)}),
-        ("R2,L2", {(0, 0), (1, 0), (2, 0)}),
-        ("L2,R2", {(0, 0), (-1, 0), (-2, 0)}),
-        ("U10,D10", {(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10)}),
-        ("L2,R2,U2", {(0, 0), (-1, 0), (-2, 0), (0, 1), (0, 2)}),
-        ("L2,R2,U2,D2", {(0, 0), (-1, 0), (-2, 0), (0, 1), (0, 2)}),
-        ("L2,R2,D2,U2", {(0, 0), (-1, 0), (-2, 0), (0, -1), (0, -2)}),
-        ("L0", {(0, 0)}),
-        ("L1,R0", {(0, 0), (-1, 0)}),
+        ("U2", {(0, 0, 0), (0, 1, 1), (0, 2, 2)}),
+        ("D2", {(0, 0, 0), (0, -1, 1), (0, -2, 2)}),
+        ("L2", {(0, 0, 0), (-1, 0, 1), (-2, 0, 2)}),
+        #("R2", {(0, 0), (1, 0), (2, 0)}),
+        #("U2,D2", {(0, 0), (0, 1), (0, 2)}),
+        #("D2,U2", {(0, 0), (0, -1), (0, -2)}),
+        #("R2,L2", {(0, 0), (1, 0), (2, 0)}),
+        #("L2,R2", {(0, 0), (-1, 0), (-2, 0)}),
+        ("U10,D10", {(0, 0, 0), (0, 1, 1), (0, 2, 2), (0, 3, 3), (0, 4, 4), (0, 5, 5), (0, 6, 6), (0, 7, 7), (0, 8, 8), (0, 9, 9), (0, 10, 10)}),
+        ("L2,R2,U2", {(0, 0, 0), (-1, 0, 1), (-2, 0, 2), (0, 1, 5), (0, 2, 6)}),
+        #("L2,R2,U2,D2", {(0, 0), (-1, 0), (-2, 0), (0, 1), (0, 2)}),
+        #("L2,R2,D2,U2", {(0, 0), (-1, 0), (-2, 0), (0, -1), (0, -2)}),
+        #("L0", {(0, 0)}),
+        #("L1,R0", {(0, 0), (-1, 0)}),
     ]
 )
 def test_transmogrification(test, expectation):
     assert dec3.setrogify_wiring(test) == expectation
 
-
 @pytest.mark.parametrize(
     'test, expectation',
     [
-        (["U2", "D2"], {(0, 0)}),
-        (["U2", "U2"], {(0, 0), (0, 1), (0, 2)}),
-        (["U2", "R1,U2,L1"], {(0, 0), (0, 2)}),
-    ]
-)
-def test_xrayed(test, expectation):
-    wiring1, wiring2 = test
-    layout1 = dec3.setrogify_wiring(wiring1)
-    layout2 = dec3.setrogify_wiring(wiring2)
-    intersection = layout1.intersection(layout2)
-    assert intersection == expectation
-
-@pytest.mark.parametrize(
-    'test, expectation',
-    [
-        ({(0, 0), (0, 1)}, 1),
-        ({(0, 0), (0, 1), (0, 2)}, 1),
-        ({(0, 0), (0, 2)}, 2),
+        (('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), 610),
+        (('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 410),
     ]
 )
 def test_santas_solver_works(test, expectation):
-    assert dec3.solve_for_santa(test) == expectation
+    def hailmary(accumulator, value):
+        x, y, distance = value
+        accumulator[(x, y)] = distance
+        return accumulator
+
+    wiring1, wiring2 = test
+    layout1 = dec3.setrogify_wiring(wiring1)
+    layout2 = dec3.setrogify_wiring(wiring2)
+
+    run1 = functools.reduce(
+        hailmary,
+        layout1,
+        {}
+    )
+    run2 = functools.reduce(
+        hailmary,
+        layout2,
+        {}
+    )
+
+    intersections = run1.keys() & run2.keys()
+    intersections.remove((0, 0))
+
+    def solver(accumulator, value):
+        combined_distance = run1[value] + run2[value]
+
+        if accumulator is None:
+            return combined_distance
+        elif combined_distance < accumulator:
+            return combined_distance
+        return accumulator
+
+    return functools.reduce(
+        solver,
+        intersections,
+        None
+    )
