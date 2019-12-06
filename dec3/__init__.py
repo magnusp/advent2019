14464eb6bd506616f32b2e7caa12addf0a7a5f70
Magnus Persson <magnus.e.persson@gmail.com>
2019-12-06 15:54:24 +0100

    Relief!

--- dec3/__init__.py
+++ dec3/__init__.py
@@ -1,85 +1,53 @@
 import itertools
-from typing import Set
-
-
-def use_hex(wiring, x: int, y: int, travelled: int) -> tuple:
-    maybe_tuple = filter(
-        lambda t: t[0] == x and t[1] == y,
-        wiring
-    )
-    try:
-        return next(maybe_tuple)
-    except StopIteration:
-        return x, y, travelled
-
-
-def setrogify_wiring(directions):
-    wiring = {(0, 0, 0)}
-    bug = (0, 0, 0)
-
-    for movement in directions.split(','):
-        bug_x, bug_y, distance_traveled = bug
-        direction, distance = [movement[0], int(movement[1:])]
-        line = ()
-        if direction is 'U':
-            line = [use_hex(wiring, bug_x, q, p) for q, p in zip(range(bug_y, bug_y + distance + 1), itertools.count(distance_traveled))]
-            bug_y = bug_y + distance
-        elif direction is 'D':
-            line = [use_hex(wiring, bug_x, q, p) for q, p in zip(range(bug_y, bug_y - distance - 1, -1), itertools.count(distance_traveled))]
-            bug_y = bug_y - distance
-        elif direction is 'L':
-            line = [use_hex(wiring, q, bug_y, p) for q, p in zip(range(bug_x, bug_x - distance - 1, -1), itertools.count(distance_traveled))]
-            bug_x = bug_x - distance
-        elif direction is 'R':
-            line = [use_hex(wiring, q, bug_y, p) for q, p in zip(range(bug_x, bug_x + distance + 1), itertools.count(distance_traveled))]
-            bug_x = bug_x + distance
-        else:
-            raise RuntimeError(f"Bug cant move that way: {direction}")
-        wiring = wiring.union(line)
-        bug = (bug_x, bug_y, distance_traveled + distance)
-
-    return wiring
-
-
-def solve_for_santa(intersection: Set) -> int:
-    def keyFunction(element):
-        return abs(element[0]) + abs(element[1])
-
-    smaller_set = intersection.difference({(0, 0)})
-    gingerbread_location = min(smaller_set, key=keyFunction)
-    return keyFunction(gingerbread_location)
+from collections import defaultdict
 
 
 def make_vector_ranges(wiring):
-    zero_cycle = itertools.cycle([0])
-    positive_cycle = itertools.cycle([1])
-    negative_cycle = itertools.cycle([-1])
+    vector_mapping = {
+        'U': (0, 1),
+        'D': (0, -1),
+        'L': (-1, 0),
+        'R': (1, 0)
+    }
 
     def make_vector(item):
         direction, distance = (item[0], int(item[1:]))
-        if direction is 'U':
-            change = (zero_cycle, positive_cycle)
-        elif direction is 'D':
-            change = (zero_cycle, negative_cycle)
-        elif direction is 'L':
-            change = (negative_cycle, zero_cycle)
-        elif direction is 'R':
-            change = (positive_cycle, zero_cycle)
-        else:
-            raise ValueError
-
-        return itertools.islice(itertools.cycle(change), 0, distance)
+        return itertools.repeat(vector_mapping[direction], distance)
 
     return [make_vector(x) for x in wiring.split(',')]
 
+
 def build(layout):
-    d = {}
+    d = defaultdict(int)
     bug = (0, 0)
+    counter = itertools.count(1, 1)
     for instruction in layout:
-        bug_x, bug_y = bug
-        flurp = list(instruction)
-        i = 0
+        for delta_x, delta_y in instruction:
+            bug = (bug[0] + delta_x, bug[1] + delta_y)
+            d[bug] = next(counter)
+
+    if (0, 0) in d:
+        del d[0, 0]
+    return d
 
 
-def solve_for_santa_but_better(wiring1, wiring2):
-    pass
+def find_nearest_intersection(layout1, layout2):
+    def keyFunc(intersection):
+        return abs(intersection[0]) + abs(intersection[1])
+
+    closest_intersection = min(
+        layout1.keys() & layout2.keys(),
+        key=keyFunc
+    )
+    return keyFunc(closest_intersection)
+
+
+def find_nearest_by_distance_travelled_intersection(layout1, layout2):
+    def keyFunc(intersection):
+        return layout1[intersection] + layout2[intersection]
+
+    closest_intersection = min(
+        layout1.keys() & layout2.keys(),
+        key=keyFunc
+    )
+    return keyFunc(closest_intersection)
--- dec3/test_dec3.py
+++ dec3/test_dec3.py
@@ -1,9 +1,20 @@
-import functools
-import itertools
-from collections import defaultdict
-
 import pytest
 import dec3
+
+@pytest.mark.parametrize(
+    'test, expectation',
+    [
+        (('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), 159),
+        (('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 135),
+    ]
+)
+def test_santas_nearest_solver_works(test, expectation):
+    wiring1, wiring2 = test
+
+    layout1 = dec3.build(dec3.make_vector_ranges(wiring1))
+    layout2 = dec3.build(dec3.make_vector_ranges(wiring2))
+    assert dec3.find_nearest_intersection(layout1, layout2) == expectation
+
 
 @pytest.mark.parametrize(
     'test, expectation',
@@ -12,7 +23,9 @@ import dec3
         (('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 410),
     ]
 )
-def test_santas_solver_works(test, expectation):
+def test_santas_nearest_by_distance_travelled_solver_works(test, expectation):
     wiring1, wiring2 = test
-    layout1 = dec3.make_vector_ranges(wiring1)
-    dec3.build(layout1)
+
+    layout1 = dec3.build(dec3.make_vector_ranges(wiring1))
+    layout2 = dec3.build(dec3.make_vector_ranges(wiring2))
+    dec3.find_nearest_by_distance_travelled_intersection(layout1, layout2)
